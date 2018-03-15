a562195260290820ee14bbe48d1b87bab536f1e4
xurongzhong <xurongzhong@126.com>
2018-03-16 00:25:21 +0800

    Add bj_sz_photo_compare in photos.py

--- photos.py
+++ photos.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Author:    xurongzhong#126.com wechat:pythontesting qq:37391319
 # CreateDate: 2018-3-15
@@ -21,13 +21,33 @@ def mark_image(filename, dst, poses):
     d = ImageDraw.Draw(image, 'RGBA')
     for color in poses:
         d.rectangle(poses[color],outline=color)   
-    image.save("{}{}{}".format(dst, os.sep, os.path.basename(filename)))
-    
-def rotate(files, dst, value=180):
-    for file_ in files:
-        image = Image.open(file_)
-        image = image.rotate(value,resample=Image.BICUBIC, expand=1)
-        image.save("{}{}{}".format(dst, os.sep, os.path.basename(file_)))    
+    image.save(dst)
+
+def mark_images(files, out,shenzhens, beijings):
+    for filename in files:
+        name = os.path.basename(filename)
+        sz = shenzhens.get(name)
+        bj = beijings.get(name)  
+        bj_sz_photo_compare(sz,bj,filename,prefix=out)
+
+def bj_sz_photo_compare(sz,bj,filename,prefix="_"):
+    poses = {}
+    if sz:
+        poses["green"] = sz
+        print("shenzhen:",sz)
+    else:
+        print("shenzhen: Can not recognize!",)
+    if bj:
+        poses["red"] = bj
+        print("beijing:",bj)
+    else:
+        print("beijing: Can not recognize!",)
+    name = os.path.basename(filename)
+    if type(name) == bytes:
+        name = name.decode()   
+    new_name = "{}{}".format(prefix,name)
+    print("Please see {}".format(new_name))
+    mark_image(filename,new_name,poses)    
  
 def rotateImage(image, angle):
     image_center = tuple(np.array(image.shape[1::-1]) / 2)
@@ -40,4 +60,4 @@ def rotate2(files, dst, value=90):
         img = cv2.imread(file_,0)
         result = rotateImage(img, value)
         cv2.imwrite("{}{}{}".format(dst, os.sep, os.path.basename(file_)), 
-                    result)     
+result) 
